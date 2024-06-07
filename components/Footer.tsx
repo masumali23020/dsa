4433556fa8dd35f0cba724d3bd05e26a99482109
masumali23020 <mdmasum0218@gmail.com>
2024-06-07 09:13:34 +0600

    complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -47,10 +47,8 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <Link href={info.url}>
-                <a target="_blank">
-                  <Image src={info.img} alt="icons" width={20} height={20} />
-                </a>
+              <Link href={info.url} target="_blank">
+                <Image src={info.img} alt="icons" width={20} height={20} />
               </Link>
             </div>
           ))}
