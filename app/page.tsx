e13321090d0944031d8b48d331ccdd144cefbc52
masumali23020 <mdmasum0218@gmail.com>
2025-01-19 07:42:34 +0600

    new branch creae pactice

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@
 
 import { navItems } from "@/data";
 
-import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
-import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
+import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
