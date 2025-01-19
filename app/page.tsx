50ff87b3d3a122578c446e42ab67c2a6d418807c
masumali23020 <mdmasum0218@gmail.com>
2025-01-19 07:46:31 +0600

    Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,7 @@
 
 import { navItems } from "@/data";
 
-import Approach from "@/components/Approach";
-import Clients from "@/components/Clients";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
@@ -17,12 +11,12 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid />
+        {/* <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
         <Approach />
-        <Footer />
+        <Footer /> */}
       </div>
     </main>
   );
